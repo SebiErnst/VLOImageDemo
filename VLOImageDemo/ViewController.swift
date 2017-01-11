779628059fd2a53c1d1eb932c33038ace2d0a438
Sebastian Ernst <ernst@agh.edu.pl>
2017-01-11 16:21:21 +0100

    initial commit

--- VLOImageDemo/ViewController.swift
+++ VLOImageDemo/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
-
+    
+    var image: UIImage!
+    
+    @IBOutlet weak var URLTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func goButtonPressed(_ sender: Any) {
+        image = UIImage(ciImage: CIImage(contentsOf: URL(string: URLTextField.text!)!)!)
+        imageView.image = image
+    }
+    
+    @IBAction func detectFaces(_ sender: Any) {
+        let detector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        let faces = detector!.features(in: image.ciImage!)
+        debugPrint(faces[0].bounds)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
